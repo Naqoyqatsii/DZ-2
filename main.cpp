edc8dc2eea1df913a23bb26c1b86cd179701d89c
Igor Rogozin <827779@gmail.com>
2023-05-07 10:48:02 +0300

    07.05.2023

---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+std::vector<std::string> split(const std::string &str, char d) {
+    std::vector<std::string> r;
+
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while (stop != std::string::npos) {
+        r.push_back(str.substr(start, stop - start));
+
+        start = stop + 1;
+        stop = str.find_first_of(d, start);
+    }
+
+    r.push_back(str.substr(start));
+
+    return r;
+}
+
+bool compare(std::vector<std::string> &a, std::vector<std::string> &b) {
+    for (int i = 0; i < 4; i++) {
+        if (a[i] != b[i]) {
+            return stoi(a[i]) > stoi(b[i]);
+        }
+    }
+    return false;
+}
+
+void separattor() {
+    std::cout << std::endl;
+    std::cout << "********************************************" << std::endl;
+    std::cout << std::endl;
+}
+
+int main(int argc, char const *argv[]) {
+    try {
+
+        std::vector<std::vector<std::string> > ip_pool;
+       
+
+        for(std::string line; std::getline(std::cin, line);)
+        {
+            std::vector<std::string> v = split(line, '\t');
+            ip_pool.push_back(split(v.at(0), '.'));
+        }
+
+
+        // TODO reverse lexicographically sort
+        sort(ip_pool.begin(), ip_pool.end(), compare);
+
+
+        for (std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip) {
+            for (std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part) {
+                if (ip_part != ip->cbegin()) {
+                    std::cout << ".";
+                }
+                std::cout << *ip_part;
+            }
+            std::cout << std::endl;
+        }
+
+        separattor();
+
+        // TODO filter by first byte and output
+        // ip = filter(1)
+        for (const auto &addr: ip_pool) {
+            if (std::stoi(addr[0]) == 1) {
+                std::cout << addr[0] << "." << addr[1] << "." << addr[2] << "." << addr[3] << std::endl;
+            }
+        }
+
+        separattor();
+
+        for (const auto &addr: ip_pool) {
+            if (std::stoi(addr[0]) == 46 && std::stoi(addr[1]) == 70) {
+                std::cout << addr[0] << "." << addr[1] << "." << addr[2] << "." << addr[3] << std::endl;
+            }
+        }
+
+        separattor();
+
+        for (const auto &addr: ip_pool) {
+            if (find(addr.begin(), addr.end(), "46") != addr.end()) {
+                std::cout << addr[0] << "." << addr[1] << "." << addr[2] << "." << addr[3] << std::endl;
+            }
+        }
+
+    }
+    catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
+
