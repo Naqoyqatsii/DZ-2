68727616dabb568200fc089b3826dbb83992492f
Igor Rogozin <827779@gmail.com>
2023-05-10 19:48:40 +0300

    test

--- main.cpp
+++ main.cpp
@@ -4,24 +4,23 @@
 #include <fstream>
 #include <algorithm>
 
-std::vector<int> split(const std::string &str, char d) {
-    std::vector<int> r;
-
+std::array<u_int8_t, 4> split(const std::string &str, char d) {
+    std::array<u_int8_t, 4> r;
+    int i = 0;
     std::string::size_type start = 0;
     std::string::size_type stop = str.find_first_of(d);
     while (stop != std::string::npos) {
-        r.push_back(std::stoi(str.substr(start, stop - start)));
-
-        start = stop + 1;
+        r[i] = std::stoi(str.substr(start, stop - start));
+        start = stop + 1; i++;
         stop = str.find_first_of(d, start);
     }
 
-    r.push_back(std::stoi(str.substr(start)));
+    r[i] = std::stoi(str.substr(start));
 
     return r;
 }
 
-bool compare(std::vector<int> &a, std::vector<int> &b) {
+bool compare(std::array<u_int8_t, 4> &a, std::array<u_int8_t, 4> &b) {
     for (int i = 0; i < 4; i++) {
         if (a[i] != b[i]) {
             return a[i] > b[i];
@@ -39,12 +38,12 @@ void separattor() {
 int main(int argc, char const *argv[]) {
     try {
 
-        std::vector<std::vector<int> > ip_pool;
+        std::vector<std::array<u_int8_t, 4> > ip_pool;
        
 
         for(std::string line; std::getline(std::cin, line);)
         {
-            std::vector<int> v = split(line, '\t');
+            std::array<u_int8_t, 4> v = split(line, '\t');
             ip_pool.push_back(split(std::to_string(v.at(0)), '.'));
         }
 
@@ -53,8 +52,8 @@ int main(int argc, char const *argv[]) {
         sort(ip_pool.begin(), ip_pool.end(), compare);
 
 
-        for (std::vector<std::vector<int> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip) {
-            for (std::vector<int>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part) {
+        for (std::vector<std::array<u_int8_t, 4> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip) {
+            for (std::array<u_int8_t, 4>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part) {
                 if (ip_part != ip->cbegin()) {
                     std::cout << ".";
                 }
@@ -84,7 +83,7 @@ int main(int argc, char const *argv[]) {
         separattor();
 
         for (const auto &addr: ip_pool) {
-            if (find(addr.begin(), addr.end(), 46) != addr.end()) {
+            if (std::find(addr.begin(), addr.end(), 46) != addr.end()) {
                 std::cout << addr[0] << "." << addr[1] << "." << addr[2] << "." << addr[3] << std::endl;
             }
         }
